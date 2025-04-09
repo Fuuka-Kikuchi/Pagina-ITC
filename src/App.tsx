3e6c3e285adece0a0004cf9fa4b4ecdf61a7cc9a
fuuka-Kikuchi <fuuka0kikuchi@gmail.com>
2025-04-09 00:49:53 -0600

    borrar

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ const App: React.FC = () => {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
-                <Route path="/instituto" element={<Instituto />} />
+         <Route path="/instituto" element={<Instituto />} />
 
      
         {/* Aquí puedes agregar más rutas según lo necesites */}
